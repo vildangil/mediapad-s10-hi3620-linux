1040b72f0b8120c87178fdfa718b2efedcde2165
VildanG <gildanovvildan0@gmail.com>
2026-09-12 12:22:39 +0300

    drm: hi3620: fix static DRM mode initializer

--- drivers/gpu/drm/hisilicon/hi3620/hi3620_edc_drm.c
+++ drivers/gpu/drm/hisilicon/hi3620/hi3620_edc_drm.c
@@ -58,11 +58,12 @@ struct hi3620_edc {
 	void __iomem *regs;
 };
 
-static const struct drm_display_mode hi3620_panel_mode =
+static const struct drm_display_mode hi3620_panel_mode = {
 	DRM_MODE("1920x1200", DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED,
 		 150000, 1920, 2009, 2014, 2063, 0,
 		 1200, 1206, 1208, 1212, 0,
-		 DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_PVSYNC);
+		 DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_PVSYNC)
+};
 
 static inline struct hi3620_edc *pipe_to_hi3620(struct drm_simple_display_pipe *pipe)
 {
@@ -144,59 +145,30 @@ static void hi3620_pipe_enable(struct drm_simple_display_pipe *pipe,
 			       struct drm_crtc_state *crtc_state)
 {
 	struct hi3620_edc *edc = pipe_to_hi3620(pipe);
-	u32 disp = readl(edc->regs + EDC_DISP_CTL);
 
-	/* Do not reinitialise the live display pipeline. Merely verify takeover. */
-	dev_info(edc->drm->dev,
-		 "KMS takeover: EDC display %s, DISP_CTL=%08x CH1=%08x stride=%u\n",
-		 (disp & EDC_DISP_CTL_ENABLE) ? "already enabled" : "not enabled",
-		 disp, readl(edc->regs + EDC_CH1L_ADDR),
-		 readl(edc->regs + EDC_CH1_STRIDE) & 0x3fff);
+	hi3620_edc_program_plane(edc, pipe->plane.state);
 }
 
 static void hi3620_pipe_disable(struct drm_simple_display_pipe *pipe)
 {
-	struct hi3620_edc *edc = pipe_to_hi3620(pipe);
-
-	/*
-	 * Stage 1 intentionally leaves EDC/LDI/DSI running.  This makes VT/Xorg
-	 * failures recoverable through the bootloader framebuffer/simplefb path.
-	 */
-	dev_dbg(edc->drm->dev, "KMS disable requested; preserving live EDC pipeline\n");
+	/* Keep the bootloader-programmed pipeline alive during initial takeover. */
 }
 
 static void hi3620_pipe_update(struct drm_simple_display_pipe *pipe,
 			       struct drm_plane_state *old_state)
 {
 	struct hi3620_edc *edc = pipe_to_hi3620(pipe);
-	struct drm_plane_state *new_state = pipe->plane.state;
-	struct drm_crtc *crtc = &pipe->crtc;
-	unsigned long flags;
 
-	/* drm_plane_helper atomic_update receives old_state; hardware needs new. */
-	hi3620_edc_program_plane(edc, new_state);
-
-	/* No vblank IRQ in takeover stage: complete atomic events immediately. */
-	if (crtc->state->event) {
-		spin_lock_irqsave(&edc->drm->event_lock, flags);
-		drm_crtc_send_vblank_event(crtc, crtc->state->event);
-		crtc->state->event = NULL;
-		spin_unlock_irqrestore(&edc->drm->event_lock, flags);
-	}
+	/* Atomic helpers have already installed the new state in plane.state. */
+	hi3620_edc_program_plane(edc, pipe->plane.state);
 }
 
 static const struct drm_simple_display_pipe_funcs hi3620_pipe_funcs = {
+	.check = hi3620_pipe_check,
 	.enable = hi3620_pipe_enable,
 	.disable = hi3620_pipe_disable,
-	.check = hi3620_pipe_check,
 	.update = hi3620_pipe_update,
 };
-
-static enum drm_connector_status
-hi3620_connector_detect(struct drm_connector *connector, bool force)
-{
-	return connector_status_connected;
-}
 
 static int hi3620_connector_get_modes(struct drm_connector *connector)
 {
@@ -210,125 +182,49 @@ static int hi3620_connector_get_modes(struct drm_connector *connector)
 	return 1;
 }
 
-static void hi3620_connector_destroy(struct drm_connector *connector)
-{
-	drm_connector_unregister(connector);
-	drm_connector_cleanup(connector);
-}
-
 static const struct drm_connector_helper_funcs hi3620_connector_helper_funcs = {
 	.get_modes = hi3620_connector_get_modes,
 };
 
+static enum drm_connector_status
+hi3620_connector_detect(struct drm_connector *connector, bool force)
+{
+	return connector_status_connected;
+}
+
 static const struct drm_connector_funcs hi3620_connector_funcs = {
-	.dpms = drm_helper_connector_dpms,
-	.reset = drm_atomic_helper_connector_reset,
+	.dpms = drm_atomic_helper_connector_dpms,
 	.detect = hi3620_connector_detect,
 	.fill_modes = drm_helper_probe_single_connector_modes,
-	.destroy = hi3620_connector_destroy,
+	.destroy = drm_connector_cleanup,
+	.reset = drm_atomic_helper_connector_reset,
 	.atomic_duplicate_state = drm_atomic_helper_connector_duplicate_state,
 	.atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
 };
 
-static void hi3620_output_poll_changed(struct drm_device *drm)
+static const uint32_t hi3620_formats[] = {
+	DRM_FORMAT_XRGB8888,
+	DRM_FORMAT_ARGB8888,
+};
+
+static void hi3620_drm_output_poll_changed(struct drm_device *drm)
 {
-	/* Fixed internal panel: nothing to reprobe. */
+	struct hi3620_edc *edc = drm->dev_private;
+
+	drm_fbdev_cma_hotplug_event(edc->drm->fb_helper);
 }
 
 static const struct drm_mode_config_funcs hi3620_mode_config_funcs = {
 	.fb_create = drm_fb_cma_create,
-	.output_poll_changed = hi3620_output_poll_changed,
+	.output_poll_changed = hi3620_drm_output_poll_changed,
 	.atomic_check = drm_atomic_helper_check,
 	.atomic_commit = drm_atomic_helper_commit,
 };
 
-static const struct file_operations hi3620_drm_fops = {
-	.owner = THIS_MODULE,
-	.open = drm_open,
-	.release = drm_release,
-	.unlocked_ioctl = drm_ioctl,
-#ifdef CONFIG_COMPAT
-	.compat_ioctl = drm_compat_ioctl,
-#endif
-	.poll = drm_poll,
-	.read = drm_read,
-	.llseek = no_llseek,
-	.mmap = drm_gem_cma_mmap,
-};
-
-static struct drm_driver hi3620_drm_driver = {
-	.driver_features = DRIVER_MODESET | DRIVER_GEM | DRIVER_PRIME | DRIVER_ATOMIC,
-	.fops = &hi3620_drm_fops,
-	.dumb_create = drm_gem_cma_dumb_create,
-	.dumb_map_offset = drm_gem_cma_dumb_map_offset,
-	.dumb_destroy = drm_gem_dumb_destroy,
-	.gem_free_object_unlocked = drm_gem_cma_free_object,
-	.gem_vm_ops = &drm_gem_cma_vm_ops,
-	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
-	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
-	.gem_prime_export = drm_gem_prime_export,
-	.gem_prime_import = drm_gem_prime_import,
-	.gem_prime_get_sg_table = drm_gem_cma_prime_get_sg_table,
-	.gem_prime_import_sg_table = drm_gem_cma_prime_import_sg_table,
-	.gem_prime_vmap = drm_gem_cma_prime_vmap,
-	.gem_prime_vunmap = drm_gem_cma_prime_vunmap,
-	.gem_prime_mmap = drm_gem_cma_prime_mmap,
-	.name = "hi3620-edc",
-	.desc = "HiSilicon Hi3620 EDC0 takeover KMS",
-	.date = "20260912",
-	.major = 0,
-	.minor = 1,
-};
-
-static int hi3620_edc_probe(struct platform_device *pdev)
+static int hi3620_drm_load(struct drm_device *drm, unsigned long flags)
 {
-	static const u32 formats[] = {
-		DRM_FORMAT_XRGB8888,
-		DRM_FORMAT_ARGB8888,
-	};
-	struct hi3620_edc *edc;
-	struct drm_device *drm;
-	struct resource *res;
+	struct hi3620_edc *edc = drm->dev_private;
 	int ret;
-
-	if (!of_machine_is_compatible("huawei,s10-101x"))
-		return -ENODEV;
-
-	drm = drm_dev_alloc(&hi3620_drm_driver, &pdev->dev);
-	if (IS_ERR(drm))
-		return PTR_ERR(drm);
-
-	edc = devm_kzalloc(&pdev->dev, sizeof(*edc), GFP_KERNEL);
-	if (!edc) {
-		ret = -ENOMEM;
-		goto err_unref;
-	}
-
-	edc->drm = drm;
-	drm->dev_private = edc;
-
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	edc->regs = devm_ioremap_resource(&pdev->dev, res);
-	if (IS_ERR(edc->regs)) {
-		ret = PTR_ERR(edc->regs);
-		goto err_unref;
-	}
-
-	if (dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(32))) {
-		ret = -ENODEV;
-		goto err_unref;
-	}
-
-	dev_info(&pdev->dev,
-		 "boot EDC: addr=%08x stride=%u size=%08x ch1ctl=%08x disp=%08x sts=%08x ints=%08x inte=%08x\n",
-		 readl(edc->regs + EDC_CH1L_ADDR),
-		 readl(edc->regs + EDC_CH1_STRIDE) & 0x3fff,
-		 readl(edc->regs + EDC_CH1_SIZE),
-		 readl(edc->regs + EDC_CH1_CTL),
-		 readl(edc->regs + EDC_DISP_CTL),
-		 readl(edc->regs + EDC_STS),
-		 readl(edc->regs + EDC_INTS),
-		 readl(edc->regs + EDC_INTE));
 
 	drm_mode_config_init(drm);
 	drm->mode_config.min_width = 1920;
@@ -337,54 +233,114 @@ static int hi3620_edc_probe(struct platform_device *pdev)
 	drm->mode_config.max_height = 1200;
 	drm->mode_config.funcs = &hi3620_mode_config_funcs;
 
-	edc->connector.display_info.width_mm = 217;
-	edc->connector.display_info.height_mm = 136;
-	drm_connector_helper_add(&edc->connector, &hi3620_connector_helper_funcs);
 	ret = drm_connector_init(drm, &edc->connector, &hi3620_connector_funcs,
 				 DRM_MODE_CONNECTOR_DSI);
 	if (ret)
-		goto err_mode_config;
+		goto err_config;
+
+	drm_connector_helper_add(&edc->connector, &hi3620_connector_helper_funcs);
 
 	ret = drm_simple_display_pipe_init(drm, &edc->pipe, &hi3620_pipe_funcs,
-					   formats, ARRAY_SIZE(formats),
+					   hi3620_formats, ARRAY_SIZE(hi3620_formats),
 					   &edc->connector);
 	if (ret)
-		goto err_mode_config;
+		goto err_connector;
 
 	drm_mode_config_reset(drm);
+
+	ret = drm_vblank_init(drm, 1);
+	if (ret)
+		goto err_connector;
+
+	ret = drm_fbdev_cma_init(drm, 32, 1, 1);
+	if (ret)
+		goto err_connector;
+
 	drm_kms_helper_poll_init(drm);
 
-	ret = drm_dev_register(drm, 0);
-	if (ret)
-		goto err_poll;
-
-	platform_set_drvdata(pdev, drm);
-	dev_info(&pdev->dev,
-		 "registered takeover KMS; simplefb intentionally retained as fallback\n");
+	dev_info(drm->dev,
+		 "Hi3620 EDC0 KMS takeover ready: disp_ctl=%08x size=%08x sts=%08x ints=%08x inte=%08x\n",
+		 readl(edc->regs + EDC_DISP_CTL), readl(edc->regs + EDC_DISP_SIZE),
+		 readl(edc->regs + EDC_STS), readl(edc->regs + EDC_INTS),
+		 readl(edc->regs + EDC_INTE));
 	return 0;
 
-err_poll:
-	drm_kms_helper_poll_fini(drm);
-err_mode_config:
+err_connector:
+	drm_connector_cleanup(&edc->connector);
+err_config:
 	drm_mode_config_cleanup(drm);
-err_unref:
-	drm_dev_unref(drm);
 	return ret;
+}
+
+static int hi3620_drm_unload(struct drm_device *drm)
+{
+	drm_kms_helper_poll_fini(drm);
+	drm_fbdev_cma_fini(drm);
+	drm_mode_config_cleanup(drm);
+	return 0;
+}
+
+static struct drm_driver hi3620_drm_driver = {
+	.driver_features = DRIVER_GEM | DRIVER_MODESET | DRIVER_ATOMIC,
+	.load = hi3620_drm_load,
+	.unload = hi3620_drm_unload,
+	.gem_free_object = drm_gem_cma_free_object,
+	.gem_vm_ops = &drm_gem_cma_vm_ops,
+	.dumb_create = drm_gem_cma_dumb_create,
+	.dumb_map_offset = drm_gem_cma_dumb_map_offset,
+	.dumb_destroy = drm_gem_dumb_destroy,
+	.fops = &drm_fops,
+	.name = "hi3620-edc",
+	.desc = "HiSilicon Hi3620 EDC0 DRM/KMS takeover",
+	.date = "20260912",
+	.major = 0,
+	.minor = 1,
+};
+
+static int hi3620_edc_probe(struct platform_device *pdev)
+{
+	struct hi3620_edc *edc;
+	struct drm_device *drm;
+	struct resource *res;
+	int ret;
+
+	edc = devm_kzalloc(&pdev->dev, sizeof(*edc), GFP_KERNEL);
+	if (!edc)
+		return -ENOMEM;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	edc->regs = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(edc->regs))
+		return PTR_ERR(edc->regs);
+
+	drm = drm_dev_alloc(&hi3620_drm_driver, &pdev->dev);
+	if (IS_ERR(drm))
+		return PTR_ERR(drm);
+
+	edc->drm = drm;
+	drm->dev_private = edc;
+	platform_set_drvdata(pdev, edc);
+
+	ret = drm_dev_register(drm, 0);
+	if (ret) {
+		drm_dev_unref(drm);
+		return ret;
+	}
+
+	return 0;
 }
 
 static int hi3620_edc_remove(struct platform_device *pdev)
 {
-	struct drm_device *drm = platform_get_drvdata(pdev);
+	struct hi3620_edc *edc = platform_get_drvdata(pdev);
 
-	drm_dev_unregister(drm);
-	drm_kms_helper_poll_fini(drm);
-	drm_mode_config_cleanup(drm);
-	drm_dev_unref(drm);
+	drm_dev_unregister(edc->drm);
+	drm_dev_unref(edc->drm);
 	return 0;
 }
 
 static const struct of_device_id hi3620_edc_of_match[] = {
-	{ .compatible = "hisilicon,hi3620-edc-kms" },
+	{ .compatible = "hisilicon,hi3620-edc-drm" },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, hi3620_edc_of_match);
@@ -399,6 +355,6 @@ static struct platform_driver hi3620_edc_platform_driver = {
 };
 module_platform_driver(hi3620_edc_platform_driver);
 
-MODULE_AUTHOR("VildanG / postmarketOS bring-up");
-MODULE_DESCRIPTION("Experimental HiSilicon Hi3620 EDC0 DRM/KMS takeover driver");
+MODULE_AUTHOR("VildanG / OpenAI bring-up");
+MODULE_DESCRIPTION("Experimental DRM/KMS takeover driver for Huawei MediaPad 10 FHD Hi3620 EDC0");
 MODULE_LICENSE("GPL v2");
